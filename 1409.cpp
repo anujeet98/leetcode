bde2f1da329e41d55b60359d7b551da3de0d4cf1
anujeet98 <anujeet98@gmail.com>
2021-01-26 14:17:33 +0530

    medium 1409 array

---
+++ 1409.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> processQueries(vector<int>& queries, int m) {
+        vector<int> output;
+        vector<int> P;
+        for(int i=1;i<=m;i++)
+            P.push_back(i);
+        for(int i:queries){
+            auto iter=find(P.begin(),P.end(),i);
+            output.push_back(iter-P.begin());
+            P.erase(iter);        
+            P.insert(P.begin(),i);
+        }
+        return output;
+    }
+};
